a89c8ce7f069285c081c18140d8a0e3bb5bf8719
DESKTOP-L6SJCDI\Marcin <mar.czekaj11@gmail.com>
2017-08-19 00:02:04 +0200

    Add new map make in blender

--- Assets/Scripts/PlayerControler/Door/DoorOpen/DoorIsOpenedAndClosed.cs
+++ Assets/Scripts/PlayerControler/Door/DoorOpen/DoorIsOpenedAndClosed.cs
@@ -9,10 +9,16 @@ public class DoorIsOpenedAndClosed : MonoBehaviour {
     private GameObject thisDoor;
 
     [SerializeField]
-    private Transform doorOpened;
+    private Transform doorOpened1;
 
     [SerializeField]
-    private Transform doorClosed;
+    private Transform doorClosed1;
+
+    [SerializeField]
+    private Transform doorOpened2;
+
+    [SerializeField]
+    private Transform doorClosed2;
 
     [SerializeField]
     private bool doorIsOpened;
@@ -23,28 +29,34 @@ public class DoorIsOpenedAndClosed : MonoBehaviour {
 
     #region Unity System Methods
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         doorIsOpened = false;
         thisDoor = this.gameObject;
-        doorOpened = thisDoor.transform.FindChild("Position/opened").GetComponent<Transform>();
-        doorClosed = thisDoor.transform.FindChild("Position/closed").GetComponent<Transform>();
+        doorOpened1 = thisDoor.transform.FindChild("Position/opened1").GetComponent<Transform>();
+        doorClosed1 = thisDoor.transform.FindChild("Position/closed1").GetComponent<Transform>();
+        doorOpened2 = thisDoor.transform.FindChild("Position/opened2").GetComponent<Transform>();
+        doorClosed2 = thisDoor.transform.FindChild("Position/closed2").GetComponent<Transform>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         ChangeDoorState();
-	}
+    }
     #endregion
 
     #region Door Open and Close
     private void DoorOpen()
     {
-        thisDoor.transform.FindChild("RoomDoor").position = Vector3.MoveTowards(thisDoor.transform.FindChild("RoomDoor").position, doorOpened.position, openingSpeed * Time.deltaTime);
+        thisDoor.transform.FindChild("CorridorDoors1").position = Vector3.MoveTowards(thisDoor.transform.FindChild("CorridorDoors1").position, doorOpened1.position, openingSpeed * Time.deltaTime);
+        thisDoor.transform.FindChild("CorridorDoors2").position = Vector3.MoveTowards(thisDoor.transform.FindChild("CorridorDoors2").position, doorOpened2.position, openingSpeed * Time.deltaTime);
     }
 
     private void DoorClose()
     {
-        thisDoor.transform.FindChild("RoomDoor").position = Vector3.MoveTowards(thisDoor.transform.FindChild("RoomDoor").position, doorClosed.position, openingSpeed * Time.deltaTime);
+        thisDoor.transform.FindChild("CorridorDoors1").position = Vector3.MoveTowards(thisDoor.transform.FindChild("CorridorDoors1").position, doorClosed1.position, openingSpeed * Time.deltaTime);
+        thisDoor.transform.FindChild("CorridorDoors2").position = Vector3.MoveTowards(thisDoor.transform.FindChild("CorridorDoors2").position, doorClosed2.position, openingSpeed * Time.deltaTime);
     }
 
     private void ChangeDoorState()
---
+++ Assets/Scripts/PlayerControler/Door/DoorOpen/DoubleDoorOpen.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleDoorOpen : MonoBehaviour {
+
+    #region Variables
+    [SerializeField]
+    private GameObject thisDoor;
+
+    [SerializeField]
+    private Transform doorOpened1;
+
+    [SerializeField]
+    private Transform doorClosed1;
+
+    [SerializeField]
+    private Transform doorOpened2;
+
+    [SerializeField]
+    private Transform doorClosed2;
+
+    [SerializeField]
+    private bool doorIsOpened;
+
+    [SerializeField]
+    private float openingSpeed = 2.0f;
+    #endregion
+
+    #region Unity System Methods
+    // Use this for initialization
+    void Start()
+    {
+        doorIsOpened = false;
+        thisDoor = this.gameObject;
+        doorOpened1 = thisDoor.transform.FindChild("Position/opened1").GetComponent<Transform>();
+        doorClosed1 = thisDoor.transform.FindChild("Position/closed1").GetComponent<Transform>();
+        if (thisDoor.transform.FindChild("Position/opened2").GetComponent<Transform>() != null && thisDoor.transform.FindChild("Position/closed2").GetComponent<Transform>() != null)
+        {
+            doorOpened2 = thisDoor.transform.FindChild("Position/opened2").GetComponent<Transform>();
+            doorClosed2 = thisDoor.transform.FindChild("Position/closed2").GetComponent<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeDoorState();
+    }
+    #endregion
+
+    #region Door Open and Close
+    private void DoorOpen()
+    {
+        thisDoor.transform.FindChild("CorridorDoors1").position = Vector3.MoveTowards(thisDoor.transform.FindChild("CorridorDoors1").position, doorOpened1.position, openingSpeed * Time.deltaTime);
+        if(thisDoor.transform.FindChild("CorridorDoors2").position!=null)
+            thisDoor.transform.FindChild("CorridorDoors2").position = Vector3.MoveTowards(thisDoor.transform.FindChild("CorridorDoors2").position, doorOpened2.position, openingSpeed * Time.deltaTime);
+    }
+
+    private void DoorClose()
+    {
+        thisDoor.transform.FindChild("CorridorDoors1").position = Vector3.MoveTowards(thisDoor.transform.FindChild("CorridorDoors1").position, doorClosed1.position, openingSpeed * Time.deltaTime);
+        if (thisDoor.transform.FindChild("CorridorDoors2").position != null)
+            thisDoor.transform.FindChild("CorridorDoors2").position = Vector3.MoveTowards(thisDoor.transform.FindChild("CorridorDoors2").position, doorClosed2.position, openingSpeed * Time.deltaTime);
+    }
+
+    private void ChangeDoorState()
+    {
+        if (doorIsOpened)
+        {
+            DoorOpen();
+        }
+        else
+        {
+            DoorClose();
+        }
+    }
+    #endregion
+
+    #region Setter
+    public void SetDoorIsOpened()
+    {
+        this.doorIsOpened = !this.doorIsOpened;
+    }
+    #endregion
+
+    #region Getter
+    public bool GetDoorIsOpened()
+    {
+        return this.doorIsOpened;
+    }
+    #endregion
+
+
+}
